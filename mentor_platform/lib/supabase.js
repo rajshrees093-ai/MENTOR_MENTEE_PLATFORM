687742014492c4e819cf22f7418048072ab2e58a
Rajshree <rajshrees093@gmail.com>
2026-03-19 21:20:34 +0530

    day 2

--- mentor_platform/lib/supabase.js
+++ mentor_platform/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqaXRpYm9ieWFkZnRodGdsemxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5MjQwNDcsImV4cCI6MjA4OTUwMDA0N30.U7pzX2sls0hltcvsskE5QO4zzHPsyH9ASgFl70TPJMw"
-const supabaseKey = "YOUR_ANON_KEY"
+const supabaseUrl = "https://pjitibobyadfthtglzlk.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqaXRpYm9ieWFkZnRodGdsemxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5MjQwNDcsImV4cCI6MjA4OTUwMDA0N30.U7pzX2sls0hltcvsskE5QO4zzHPsyH9ASgFl70TPJMw"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
