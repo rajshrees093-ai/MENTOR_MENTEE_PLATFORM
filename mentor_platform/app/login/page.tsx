2e3e6b8fbe7959c061e0919605366146d2e05f55
Rajshree <rajshrees093@gmail.com>
2026-03-19 20:53:58 +0530

    day 2

---
+++ mentor_platform/app/login/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "@/lib/supabase";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error.message);
+    } else {
+      alert("Login successful 🚀");
+    }
+  };
+
+  return (
+    <div className="p-5">
+      <h1>Login</h1>
+
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      /><br /><br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      /><br /><br />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
