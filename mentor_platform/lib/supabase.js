67e36f5b9f1ae9b72dc95ccfd64cace0ed22d422
Rajshree <rajshrees093@gmail.com>
2026-03-20 17:29:04 +0530

    changedd

--- mentor_platform/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = "https://pjitibobyadfthtglzlk.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqaXRpYm9ieWFkZnRodGdsemxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5MjQwNDcsImV4cCI6MjA4OTUwMDA0N30.U7pzX2sls0hltcvsskE5QO4zzHPsyH9ASgFl70TPJMw"
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
